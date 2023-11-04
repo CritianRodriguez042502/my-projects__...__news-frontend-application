c79effba5ab0bc1e9c87a70bb07d87c14525ca57
Cristian Rodriguez Benitez <126631418+CritianRodriguez042502@users.noreply.github.com>
2023-11-03 19:05:29 -0500

    Agregando opacidad del inciio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Dom from "react-dom/client";
-import Redirects  from "./routes/index";
+import Redirects from "./routes/index";
 import "./styles/style.css";
 
-Dom.createRoot(document.querySelector("#root")).render(
-  <>
-    <Redirects />
-  </>
-);
+const App = () => {
+  const [view, setView] = useState(0);
+
+  useEffect(function () {
+    setTimeout(function () {
+      setView(1);
+    }, 1000);
+  }, []);
+  
+  return (
+    <article style={{ opacity: view }}>
+      <Redirects />
+    </article>
+  );
+};
+
+Dom.createRoot(document.querySelector("#root")).render(<App />);
